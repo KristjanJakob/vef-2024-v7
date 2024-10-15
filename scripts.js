95d208f741857b71410dd3b80419eb2365842efb
Kristján Jakob Ásgrímsson <kristjanjakobasgrimsson@askja-dw499.rhi.hi.is>
2024-10-15 14:48:17 +0000

    Laga .js og lagfæra smá.

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@
 //------------------------------------------------------------------------------
 // Fastar
 
+
 /** Íslenskir sérhljóðar */
 const CONSONANTS = 'bcdfghjklmnpqrstvwxz'.split('');
 
@@ -20,7 +21,7 @@ const VOWELS = 'aeiouyáéýúíóöæ'.split('');
 /**
  * Athuga hvort óþekkt gildi sé strengur eða ekki.
  * @param {unknown} str Óþekkt gildi sem athuga á hvort sé strengur.
- * @returns `true` ef `str` er strengur, annars `false`.
+ * @returns {boolean}`true` ef `str` er strengur, annars `false`.
  */
 // Skilgreinum anonymous fall og bindum við breytuna `isString`
 const isString = (str) => typeof str === 'string';
@@ -49,32 +50,88 @@ function split(str, separator = ' ') {
 // Grunnföll sem skilgreina á
 
 function longest(str) {
-  // Útfæra
+  const split = str.split(' ');
+  let lengstaOrd = '';
+  for (const ord of split) {
+    if (ord.length > lengstaOrd.length) {
+      lengstaOrd = ord;
+    }
+  }
+  return lengstaOrd;
 }
+console.assert(longest('þetta er próf') === 'þetta', 'longest: should return "þetta"');
 
 function shortest(str) {
-  // Útfæra
+  const split = str.split(' ');
+  let styrstaOrd = split[0]; // Initialize with the first word
+  for (const ord of split) {
+    if (ord.length < styrstaOrd.length) {
+      styrstaOrd = ord;
+    }
+  }
+  return styrstaOrd;
 }
+
+console.assert(shortest('þetta er próf') === 'er', 'shortest: should return "er"');
+
 
 function reverse(str) {
-  // Útfæra
+  if (isString(str)) {
+    const split = str.split('');
+    const reversed = split.reverse();
+  
+    return reversed.join('');
+  }
+  return null;
 }
+console.assert(reverse('halló') === 'óllah', 'reverse: sný við einföldum string');
+
+console.assert(reverse(false) === null, 'reverse: ef ekki strengur, skila null');
 
 function palindrome(str) {
-  // Útfæra
+  if (!isString(str)) {
+    return false;
+  }
+
+  const hreinnStrengur = str.toLowerCase().replace(/[\s.,!?]/g, ''); // Remove spaces and punctuation, lowercase
+  return hreinnStrengur === reverse(hreinnStrengur);
 }
+
+console.assert(palindrome('abba') === true, 'palindrome: "abba" er palindrome');
+console.assert(palindrome('abc') === false, 'palindrome: "abc" er ekki palindrome');
 
 function vowels(str) {
-  // Útfæra
+  if (!isString(str)) {
+    return 0;
+  }
+
+  return [...str.toLowerCase()].filter(char => VOWELS.includes(char)).length;
 }
 
+console.assert(vowels('halló') === 2, 'vowels: halló inniheldur 2 sérhljóða');
+console.assert(vowels('abc') === 1, 'vowels: abc inniheldur 1 sérhljóða');
+
 function consonants(str) {
-  // Útfæra
+  if (!isString(str)) {
+    return 0;
+  }
+
+  return [...str.toLowerCase()].filter(char => CONSONANTS.includes(char)).length;
 }
+
+console.assert(consonants('halló') === 3, 'consonants: halló inniheldur 3 samhljóða');
+console.assert(consonants('abc') === 2, 'consonants: abc inniheldur 2 samhljóða');
 
 //------------------------------------------------------------------------------
 // Leiðbeint ferli
 
 function start() {
-  // Útfæra
+  const profStrengur = 'Þetta er próf';
+
+  console.log('Lengsta orð:', longest(profStrengur));
+  console.log('Stysta orð:', shortest(profStrengur));
+  console.log('Snúa við:', reverse(profStrengur));
+  console.log('Er palindrome:', palindrome(profStrengur));
+  console.log('Sérhljóðar:', vowels(profStrengur));
+  console.log('Samhljóðar:', consonants(profStrengur));
 }
